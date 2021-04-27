34b65761f40622093eb806268c96adb05a82eea4
WONILLISM <47653005+WONILLISM@users.noreply.github.com>
2021-04-27 20:37:50 +0900

    init t_data

--- includes/minish.h
+++ includes/minish.h
@@ -5,9 +5,16 @@
 # include <stdio.h>
 # include "libft/libft.h"
 
-typedef struct	s_env {
+typedef struct	s_env
+{
 	char		*key;
 	char		*value;
 }				t_env;
+
+typedef struct	s_data
+{
+	t_list		*env_last;
+	char		**cmdlines;		// 명령어, 내용, NULL
+}				t_data;
 
 #endif
