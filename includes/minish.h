c45b0aad2e1653bff907753cf11364ddf1b9073e
WONILLISM <47653005+WONILLISM@users.noreply.github.com>
2021-04-27 18:55:19 +0900

    del minish loop

--- includes/minish.h
+++ includes/minish.h
@@ -1,4 +1,7 @@
 #ifndef MINISH_H
 # define MINISH_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
 
 #endif
--- main.c
+++ main.c
@@ -1,43 +1,35 @@
 #include "includes/minish.h"
 
-void    get_input(char **input)
+void	get_input(char **input)
 {
-        int             r_nbr;
-        int             idx;
-        int             cnt;
-        char    buf;
+	int		r_nbr;
+	int		idx;
+	int		cnt;
+	char	buf;
 
-        idx = 0;
-        cnt = 1;
-        *input = malloc(1);     // malloc error 처리
+	idx = 0;
+	cnt = 1;
+	*input = malloc(1);
 
-        while (1)
-        {
-                r_nbr = read(0, &buf, 1);
-                if (r_nbr == 0 || buf == '\n')
-                    break;
-                *(*input + idx) = buf;
-                idx++;
-                *input = realloc(*input, cnt + 1);
-                cnt++;
-        }
-        *(*input + idx) = '\0';
-        return (0);
+	while (1)
+	{
+		r_nbr = read(0, &buf, 1);
+		if (r_nbr == 0 || buf == '\n')
+			break;
+		*(*input + idx) = buf;
+		idx++;
+		*input = realloc(*input, cnt + 1);
+		cnt++;
+	}
 }
 
-int             minish_loop()
+int		main(int argc, char **argv, char **envv)
 {
-        char    *input;
+	char	*input;
 
-        while (1)
-        {
-            get_input(&input);
-        }
-        return (0);
-}
-
-int             main(int argc, char **argv, char **envv)
-{
-        minish_loop();
-        return (0);
+	while (1)
+	{
+		get_input(&input);
+	}
+	return (0);
 }
