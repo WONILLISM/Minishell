e429f567740278109f2e29b002d8226fcf1c622d
Wonil Park <wopark@c6r8s2.42seoul.kr>
2021-05-05 11:16:59 +0900

    set struct

--- includes/minish.h
+++ includes/minish.h
@@ -11,10 +11,21 @@ typedef struct	s_env
 	char		*value;
 }				t_env;
 
+typedef struct	s_cmd
+{
+	char		**argv;		// 명령어 내용 NULL
+	int			flag;		// 0: ; or NULL	1: pipe
+	char		quote;		// stack for ' or "
+}				t_cmd;
+
 typedef struct	s_data
 {
-	t_list		*env_last;
-	char		**cmdlines;		// 명령어, 내용, NULL
+	t_list		*lst;
+	t_cmd		*cmd;
+	char		*buf;
+	int			input_idx;
+	int			buf_idx;
+	int			cmd_idx;
 }				t_data;
 
 #endif
