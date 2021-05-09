8bac2d9413160b3e6744249ea249f59e64709f4c
junghwki <69806836+junghwki@users.noreply.github.com>
2021-05-09 21:27:26 +0900

    export, unset, env 명령어 추가

--- junghwki/execute_builtin.c
+++ junghwki/execute_builtin.c
@@ -4,7 +4,7 @@ void	execute_builtin(t_list *cmd_root)
 {
 	t_list	*temp;
 	t_cmd	*temp_cmd;
-	
+
 	temp = cmd_root->next;
 	while (temp)
 	{
@@ -25,6 +25,16 @@ void	execute_builtin(t_list *cmd_root)
 		{
 			if (!temp_cmd->argv[1])
 				export_lst_print();
+			else
+				export_add(temp_cmd);
+		}
+		else if (ft_strcmp(temp_cmd->argv[0], "unset") == 0)
+		{
+			ft_unset(temp_cmd);
+		}
+		else if (ft_strcmp(temp_cmd->argv[0], "env") == 0)
+		{
+			env_lst_print();
 		}
 		else
 		{
