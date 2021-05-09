a5d80b6cf280b1a0a5312061b6e6a675cecf5967
junghwki <69806836+junghwki@users.noreply.github.com>
2021-05-09 21:27:58 +0900

    ft_unset 추가

---
+++ junghwki/ft_unset.c
@@ -0,0 +1,25 @@
+#include "../includes/minish.h"
+
+void	ft_unset(t_cmd *cmd)
+{
+	t_list	*temp;
+	t_list	*del_lst;
+	t_env	*temp_env;
+	void	(*del)();
+	int		idx;
+
+	idx = 0;
+	del = envv_lst_del;
+	while(cmd->argv[idx])
+	{
+		temp = envv_lst_find(cmd->argv[idx]);
+		if (temp)
+		{
+			del_lst = temp->next;
+			temp->next = del_lst->next;
+			ft_lstdelone(del_lst, del);
+		}
+		idx++;
+	}
+}
+
