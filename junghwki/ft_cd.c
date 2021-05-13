6719432bee1e9898d45c1bdf2b7917b5fe3d903a
Junghwan Kim <junghwki@c6r5s4.42seoul.kr>
2021-05-13 17:38:58 +0900

    buf없이 경로 찾는방식으로 변경

--- junghwki/ft_cd.c
+++ junghwki/ft_cd.c
@@ -2,7 +2,10 @@
 
 void		ft_cd(t_cmd *cmd)
 {
+	// char	*path;
 	int		ret_value;
+	// t_list	*temp;
+	// t_env	*temp_env;
 
 	ret_value = chdir(cmd->argv[1]);
 	if (ret_value < 0)
@@ -11,4 +14,24 @@ void		ft_cd(t_cmd *cmd)
 		write(1, cmd->argv[1], ft_strlen(cmd->argv[1]));
 		write(1, ": No such file or directory\n", 28);
 	}
+	// else
+	// {
+	// 	temp = get_curr_envv_lst("PWD");
+	// 	if (temp)
+	// 	{
+	// 		path = NULL;
+	// 		path = getcwd(NULL, 0);
+	// 		if (path == NULL)
+	// 			return;
+	// 		else
+	// 		{
+	// 			temp_env = temp->content;
+	// 			if (temp_env->value)
+	// 				free(temp_env->value);
+	// 			temp_env->value = path;
+	// 		}
+	// 		if (path)
+	// 			free(path);
+	// 	}
+	// }
 }
