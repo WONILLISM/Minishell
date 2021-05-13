6c084895827af73a7c58bac5ca86e4042b4c3fdf
Junghwan Kim <junghwki@c6r5s4.42seoul.kr>
2021-05-13 17:39:17 +0900

    경로가 변경되면 PATH환경변수 값도 변경되도록 추가

--- junghwki/ft_pwd.c
+++ junghwki/ft_pwd.c
@@ -1,10 +1,27 @@
 #include "../includes/minish.h"
 
+// void		ft_pwd()
+// {
+// 	char	buf[4096];
+
+// 	getcwd(buf, 4096);
+// 	write(1, buf, ft_strlen(buf));
+// 	write(1, "\n", 1);
+// }
+
 void		ft_pwd()
 {
-	char	buf[4096];
+	char	*path;
 
-	getcwd(buf, 4096);
-	write(1, buf, ft_strlen(buf));
-	write(1, "\n", 1);
+	path = NULL;
+	path = getcwd(NULL, 0);
+	if (path == NULL)
+		printf("error");
+	else
+	{
+		write(1, path, ft_strlen(path));
+		write(1, "\n", 1);
+	}
+	if (path)
+		free(path);
 }
