bd94ab30a08f21436a36b271e40b57651c5a6f81
Junghwan Kim <junghwki@c6r6s8.42seoul.kr>
2021-05-18 16:30:33 +0900

    현재경로도 실행되게 변경

--- junghwki/ft_execve.c
+++ junghwki/ft_execve.c
@@ -1,11 +1,29 @@
 #include "../includes/minish.h"
+#define READ 0
+#define WRITE 1
+
+void	run_in_current_path(t_cmd *cmd)
+{
+	char	*path;
+	char	*free_me;
+
+	path = getcwd(NULL, 0);
+	free_me = path;
+	path = ft_strjoin(path, "/");
+	free(free_me);
+	free_me = path;
+	path = ft_strjoin(path, cmd->argv[0]);
+	free(free_me);
+	execve(path, cmd->argv, NULL);
+}
 
 void	child_process(t_cmd *cmd)
 {
+	int		idx;
 	t_list	*temp;
 	t_env	*temp_env;
 	char	**path;
-	int		idx;
+	char	*free_me;
 
 	idx = 0;
 	temp = get_curr_envv_lst("PATH");
@@ -15,18 +33,29 @@ void	child_process(t_cmd *cmd)
 		path = ft_split(temp_env->value, ':');
 		while (path[idx])
 		{
+			free_me = path[idx];
 			path[idx] = ft_strjoin(path[idx], "/");
+			free(free_me);
+			free_me = path[idx];
 			path[idx] = ft_strjoin(path[idx], cmd->argv[0]);
+			free(free_me);
 			execve(path[idx], cmd->argv, NULL);
 			idx++;
 		}
 	}
+	run_in_current_path(cmd);
 }
 
 int ft_execve(t_cmd *cmd)
 {
-	pid_t pid;
+	pid_t	pid;
+	int		*fd_p;
+	int		*fd_c;
 
+	fd_p = malloc(sizeof(int) * 2);
+	fd_c = malloc(sizeof(int) * 2);
+	if (pipe(fd_p) < 0 || pipe(fd_c) < 0)
+		write(1, "Error\n", 6);
 	pid = fork();
 	if (pid < 0)
 	{
@@ -34,11 +63,17 @@ int ft_execve(t_cmd *cmd)
 	}
 	else if (pid == 0)
 	{
+		close(fd_p[WRITE]);
+		close(fd_c[READ]);
 		child_process(cmd);
 	}
 	else
 	{
+		close(fd_p[READ]);
+		close(fd_c[WRITE]);
 		wait(NULL);
 	}
+	free(fd_p);
+	free(fd_c);
 	return (0);
 }
