1bd8ffc138de5b2aa1d0cbfd069d9e45689c91cf
Junghwan Kim <junghwki@c6r6s8.42seoul.kr>
2021-05-18 16:26:51 +0900

    경로 바뀌면 환경변수 PWD도 변경되게 수정

--- junghwki/ft_cd.c
+++ junghwki/ft_cd.c
@@ -2,36 +2,37 @@
 
 void		ft_cd(t_cmd *cmd)
 {
-	// char	*path;
 	int		ret_value;
-	// t_list	*temp;
-	// t_env	*temp_env;
+	char	*path;
+	t_list	*temp;
+	t_env	*temp_env;
 
 	ret_value = chdir(cmd->argv[1]);
 	if (ret_value < 0)
 	{
-		write(1, "minishell: cd: ", 15);
-		write(1, cmd->argv[1], ft_strlen(cmd->argv[1]));
+		write(1, "minish: cd: ", 12);
+		if (cmd->argv[1])
+			write(1, cmd->argv[1], ft_strlen(cmd->argv[1]));
 		write(1, ": No such file or directory\n", 28);
 	}
-	// else
-	// {
-	// 	temp = get_curr_envv_lst("PWD");
-	// 	if (temp)
-	// 	{
-	// 		path = NULL;
-	// 		path = getcwd(NULL, 0);
-	// 		if (path == NULL)
-	// 			return;
-	// 		else
-	// 		{
-	// 			temp_env = temp->content;
-	// 			if (temp_env->value)
-	// 				free(temp_env->value);
-	// 			temp_env->value = path;
-	// 		}
-	// 		if (path)
-	// 			free(path);
-	// 	}
-	// }
+	else
+	{
+		temp = get_curr_envv_lst("PWD");
+		if (temp)
+		{
+			path = NULL;
+			path = getcwd(NULL, 0);
+			if (path == NULL)
+				printf("ft_cd path Error\n");
+			else
+			{
+				temp_env = temp->content;
+				if (temp_env->value)
+					free(temp_env->value);
+				temp_env->value = ft_strdup(path);
+			}
+			if (path)
+				free(path);
+		}
+	}
 }
