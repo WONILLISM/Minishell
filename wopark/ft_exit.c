c95747f54ff9b0c0b91a14da7cb7c95586b13372
Wonil Park <wopark@c6r7s6.42seoul.kr>
2021-05-28 20:00:42 +0900

    .

--- wopark/ft_exit.c
+++ wopark/ft_exit.c
@@ -1,12 +1,13 @@
 #include "../includes/minish.h"
 
-void	exit_return(t_cmd *cmd)
-{
-	ft_putendl_fd("error", 2);
-	exit(g_archive.exit_stat);
-}
+// void	exit_return(t_cmd *cmd)
+// {
+// 	cmd = 0;
+// 	ft_putendl_fd("error", 2);
+// 	exit(g_archive.exit_stat);
+// }
 
-void	ft_exit(t_cmd *cmd)
-{
+// void	ft_exit(t_cmd *cmd)
+// {
 
-}
+// }
