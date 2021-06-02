c68b027d9994155484dbe84fdbbc37a4a5c43eeb
Wonil Park <wopark@c6r7s6.42seoul.kr>
2021-06-02 18:05:50 +0900

    except bonus

--- wopark/terminal_handle.c
+++ wopark/terminal_handle.c
@@ -14,20 +14,10 @@ void	term_init(struct termios *term, struct termios *backup, t_cursor *cursor)
 	term->c_cc[VTIME] = 0;
 	tcsetattr(STDIN_FILENO, TCSANOW, term);
 }
-void	set_left_key(t_cursor *cursor)
-{
-	tputs(tgetstr("le", NULL), 1, ft_putchar);
-	(cursor->key_pos)--;
-}
-void	set_right_key(t_cursor *cursor)
-{
-	tputs(tgetstr("nd", NULL), 1, ft_putchar);
-	(cursor->key_pos)++;
-}
 void	set_backspace_key(t_cursor *cursor)
 {
 	tputs(tgetstr("le", NULL), 1, ft_putchar); // move left one space
-	tputs(tgetstr("dm", NULL), 2, ft_putchar); // enter delete mode
+	tputs(tgetstr("dm", NULL), 1, ft_putchar); // enter delete mode
 	tputs(tgetstr("dc", NULL), 1, ft_putchar); // delete character (P*)
 	tputs(tgetstr("ed", NULL), 1, ft_putchar); // end delete mode
 	(cursor->key_pos)--;
@@ -43,11 +33,7 @@ void	set_printable_key(t_cursor *cursor)
 }
 int		term_key_handler(t_cursor *cursor, char **input)
 {
-	if (cursor->buf == KEY_LEFT && (cursor->key_pos) > 0)
-		set_left_key(cursor);
-	else if (cursor->buf == KEY_RIGHT && (cursor->key_pos) < cursor->len)
-		set_right_key(cursor);
-	else if (cursor->buf == KEY_BACKSPACE && (cursor->key_pos) > 0)
+	if (cursor->buf == KEY_BACKSPACE && (cursor->key_pos) > 0)
 		set_backspace_key(cursor);
 	else if (ft_isprint(cursor->buf) || cursor->buf == '\n')
 	{
@@ -57,21 +43,8 @@ int		term_key_handler(t_cursor *cursor, char **input)
 		*(*input + cursor->key_pos) = cursor->buf;
 		(cursor->key_pos)++;
 		(cursor->len)++;
-		// printf("key_pos :  %d len :  %d\n", ip_var->key_pos, ip_var->len);
 	}
-	// else if (ip_var->buf == CTRL_D)
-	// 	return (0);
-	// else
-	// {
-
-	// 	// int lines = tgetnum("sc");
-	// 	// printf("%d\n", lines);
-	// 	// lines = tgetnum("co");
-	// 	// printf("%d\n", lines);
-	// 	// lines = tgetnum("ws");
-	// 	// printf("%d\n", lines);
-	// 	// write(1, &ip_var->buf, sizeof(ip_var->buf));
-	// }
-
+	else if (cursor->buf == CTRL_D)
+		return (0);
 	return (1);
 }
