801cd3b2b66b4764d5af56c8d520025a0385b6bd
WONILLISM <47653005+WONILLISM@users.noreply.github.com>
2021-06-03 16:43:04 +0900

    make double linked list functions

--- wopark/linkedlist.c
+++ wopark/linkedlist.c
@@ -1,12 +1,75 @@
 #include "../includes/minish.h"
 
-t_dllist	*ft_dllistnew(void	*data)
+t_dlnode	*ft_dll_newnode(void	*data)
 {
-	t_dllist	*node;
+	t_dlnode	*node;
 
-	if (!(node = (t_dllist *)malloc(sizeof(t_dllist))))
-		return (NULL);
-	node->prev = NULL;
+	node = (t_dlnode *)malloc(sizeof(t_dlnode));
 	node->data = data;
+	node->prev = NULL;
 	node->next = NULL;
+	return (node);
+}
+
+void		ft_dll_init(t_dllist *list)
+{
+	list->head = ft_dll_newnode(NULL);
+	list->tail = ft_dll_newnode(NULL);
+	list->head->next = list->tail;
+	list->tail->prev = list->head;
+	list->length = 0;
+}
+
+void		ft_dll_add(t_dllist *list, void *data)
+{
+	t_dlnode	*node;
+
+	node = ft_dll_newnode(data);
+	node->prev = list->tail->prev;
+	node->next = list->tail;
+	list->tail->prev->next = node;
+	list->tail->prev = node;
+	list->length++;
+}
+
+void		ft_dll_delnode(t_dllist *list, t_dlnode *node, void (*del)(void *))
+{
+	del(node->data);
+	node->prev->next = node->next;
+	node->next->prev = node->prev;
+	free(node);
+	list->length--;
+}
+
+t_dlnode	*ft_dll_find(t_dllist *list, void *data)
+{
+	t_dlnode	*ret;
+
+	ret = list->head->next;
+	while (ret != list->tail)
+	{
+		if (ret->data == data)
+			return (ret);
+		ret = ret->next;
+	}
+	return (NULL);
+}
+
+//void		ft_dll_viewlst(t_dllist *list)
+//{
+//	t_dlnode	*node;
+
+//	node = list->head->next;
+
+//}
+void		ft_dll_clear(t_dllist *list, void (*del)(void *))
+{
+	t_dlnode	*node;
+
+	node = list->head->next;
+	while (node != list->tail)
+	{
+		ft_dll_delnode(list, node, del);
+		node = list->head->next;
+	}
 }
