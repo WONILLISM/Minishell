16b757b1b1a1a38a8662153802b6ab4447bf32f4
Junghwan Kim <junghwki@c1r4s1.42seoul.kr>
2021-06-30 08:58:41 +0900

    norminette 수정중

--- junghwki/ft_exit.c
+++ junghwki/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: junghwki <junghwki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 18:24:17 by junghwki          #+#    #+#             */
-/*   Updated: 2021/06/29 19:29:52 by junghwki         ###   ########.fr       */
+/*   Updated: 2021/06/30 08:38:27 by junghwki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,42 +46,77 @@ int				char_cmp(char a, char b)
 int				arg_check(char *str)
 {
 	char		*long_long_max;
-	int			str_idx;
-	int			idx;
 	int			check_ret;
 
 	long_long_max = "9223372036854775807";
-	str_idx = 0;
-	idx = 0;
-	if (str[str_idx] == '+' || str[str_idx] == '-')
-		str_idx++;
-	if (ft_strlen(&str[str_idx]) > 19)
+	if (*str == '+' || *str == '-')
+		str++;
+	if (ft_strlen(str) > 19)
 		return (-1);
-	else if (ft_strlen(&str[str_idx]) == 19)
-		while (str[str_idx])
+	else if (ft_strlen(str) == 19)
+		while (*str)
 		{
-			check_ret = char_cmp(long_long_max[idx], str[str_idx]);
+			check_ret = char_cmp(*long_long_max, *str);
 			if (check_ret == -1)
 				return (-1);
 			else if (check_ret == 0)
 				return (1);
 			else if (check_ret == 1)
 			{
-				idx++;
-				str_idx++;
+				long_long_max++;
+				str++;
 			}
 		}
 	return (1);
 }
 
-void			ft_exit(t_cmd *cmd)
+// void			exit_errmsg(char *str)
+// {
+// 	write(2, "minish: exit: ", 14);
+// 	write(2, str, ft_strlen(str));
+// 	err_msg_print(": numeric argument required\n", 255);
+// 	exit(g_archive.exit_stat);
+// }
+
+// void			ft_exit(t_cmd *cmd, int pipe_flag)
+// {
+// 	int			idx;
+
+// 	idx = 0;
+// 	if (!(cmd->argv[1]))
+// 	{
+// 		if (!pipe_flag)
+// 			write(2, "exit\n", 5);
+// 		exit(g_archive.exit_stat);
+// 	}
+// 	if (cmd->argv[2])
+// 		err_msg_print("minish: exit: too many arguments\n", 1);
+// 	else
+// 	{
+// 		if (arg_check(cmd->argv[1]) < 0)
+// 			exit_errmsg(cmd->argv[1]);
+// 		if (cmd->argv[1][idx] == '+' || cmd->argv[1][idx] == '-')
+// 			idx++;
+// 		while (cmd->argv[1][idx])
+// 		{
+// 			if (!ft_isdigit(cmd->argv[1][idx]))
+// 				exit_errmsg(cmd->argv[1]);
+// 			idx++;
+// 		}
+// 		g_archive.exit_stat = ft_atoll(cmd->argv[1]);
+// 		if (!pipe_flag)
+// 			write(2, "exit\n", 5);
+// 		exit(g_archive.exit_stat);
+// 	}
+// }
+void			ft_exit(t_cmd *cmd, int pipe_flag)
 {
 	int			idx;
 
 	idx = 0;
 	if (!(cmd->argv[1]))
 	{
-		if (!cmd->flag)
+		if (!pipe_flag)
 			write(2, "exit\n", 5);
 		exit(g_archive.exit_stat);
 	}
@@ -95,7 +130,6 @@ void			ft_exit(t_cmd *cmd)
 		{
 			if (!ft_isdigit(cmd->argv[1][idx]) || (arg_check(cmd->argv[1]) < 0))
 			{
-				g_archive.exit_stat = 255;
 				write(2, "minish: exit: ", 14);
 				write(2, cmd->argv[1], ft_strlen(cmd->argv[1]));
 				err_msg_print(": numeric argument required\n", 255);
@@ -104,7 +138,8 @@ void			ft_exit(t_cmd *cmd)
 			idx++;
 		}
 		g_archive.exit_stat = ft_atoll(cmd->argv[1]);
-		write(1, "exit\n", 5);
+		if (!pipe_flag)
+			write(2, "exit\n", 5);
 		exit(g_archive.exit_stat);
 	}
 }
