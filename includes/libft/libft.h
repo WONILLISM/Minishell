b424818a2526a0ad1075e8ea022014cf95a5c074
Junghwan Kim <junghwki@c6r7s4.42seoul.kr>
2021-07-04 16:59:01 +0900

    libft norminette 수정

--- includes/libft/libft.h
+++ includes/libft/libft.h
@@ -3,23 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wopark <wopark@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: junghwki <junghwki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/05 17:18:39 by wopark            #+#    #+#             */
-/*   Updated: 2021/05/28 20:12:14 by wopark           ###   ########.fr       */
+/*   Updated: 2021/07/04 16:58:33 by junghwki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
-
-#include <stdlib.h>
-#include <unistd.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 typedef	struct		s_list
 {
 	void			*content;
-	struct	s_list	*next;
+	struct s_list	*next;
 }					t_list;
 
 void				ft_bzero(void *s, size_t n);
@@ -43,15 +42,15 @@ int					ft_memcmp(const void *s1, const void *s2, size_t n);
 
 char				*ft_strchr(const char *s, int c);
 char				*ft_strrchr(const char *s, int c);
-char				*ft_strnstr(const char *big, const char *little, size_t len);
+char				*ft_strnstr(
+					const char *big, const char *little, size_t len);
 char				*ft_strdup(const char *src);
 char				*ft_strndup(const char *src, size_t size);
 char				*ft_itoa(int n);
 
 size_t				ft_strlen(const char *s);
-size_t  			ft_strlcpy(char *dest, const char *src, size_t size);
+size_t				ft_strlcpy(char *dest, const char *src, size_t size);
 size_t				ft_strlcat(char *dst, const char *src, size_t size);
-
 
 char				*ft_substr(char const *s, unsigned int start, size_t len);
 char				*ft_strjoin(char const *s1, char const *s2);
@@ -66,7 +65,6 @@ void				ft_putstr_fd(char *s, int fd);
 void				ft_putendl_fd(char *s, int fd);
 void				ft_putnbr_fd(int n, int fd);
 
-
 int					ft_lstsize(t_list *lst);
 void				ft_lstadd_back(t_list **lst, t_list *new);
 void				ft_lstadd_front(t_list **lst, t_list *new);
@@ -75,6 +73,7 @@ t_list				*ft_lstnew(void *content);
 void				ft_lstdelone(t_list *lst, void (*del)(void *));
 void				ft_lstclear(t_list **lst, void (*del)(void *));
 void				ft_lstiter(t_list *lst, void (*f)(void *));
-t_list				*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list				*ft_lstmap(
+					t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
