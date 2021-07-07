85f3f15b95987e964428cb8916b32fd2d89feb8f
Junghwan Kim <junghwki@c6r7s4.42seoul.kr>
2021-07-07 14:34:47 +0900

    noreminette 수정

--- includes/libft/ft_strltrim.c
+++ includes/libft/ft_strltrim.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strltrim.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wopark <wopark@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: junghwki <junghwki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/07 19:10:26 by wopark            #+#    #+#             */
-/*   Updated: 2021/06/02 15:39:25 by wopark           ###   ########.fr       */
+/*   Updated: 2021/07/04 17:01:37 by junghwki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ char		*ft_strltrim(char const *s1, char const *set)
 	if (!set)
 		return (ft_strdup(s1));
 	start = 0;
-	len  = ft_strlen(s1);
+	len = ft_strlen(s1);
 	while (s1[start] && ft_strchr(set, s1[start]))
 		start++;
 	if (!(res = (char *)malloc(sizeof(char) * (len - start + 1))))
